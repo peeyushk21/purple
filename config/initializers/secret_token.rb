039652ac4c1d2c7cd3990f9b6dd84f394422dd90
Peeyush <pshkmr96@gmail.com>
2014-09-27 14:36:37 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Purple::Application.config.secret_key_base = 'bd53b8b2e9e487b007a821a93f130f7b2a283b61aa12607f7ffec3d27060f5743cdda166e3d30c02f3147537b73e64ce932bf2e54f09d4566f6e9341e7f0a7b9'
